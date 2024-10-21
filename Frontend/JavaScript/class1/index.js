575da59d94972b9a9230f80484a16cf81c1a71f0
AlishaBeuria <alishabeuria905@gmail.com>
2024-10-21 21:39:03 +0530

    this my mern stack project

---
+++ Frontend/JavaScript/class1/index.js
@@ -0,0 +1,17 @@
+//var name="alisha";
+//let age=23;
+//const birthyear=2002;
+//console.log(name);
+//console.log(age,name,birthyear);
+
+//const city="Bhubaneswar"
+console.log(address);
+var address="Bhubaneswar";
+console.log(address);
+
+let user=null;
+console.log(typeof null);//object
+console.log(user);
+
+
+ 
