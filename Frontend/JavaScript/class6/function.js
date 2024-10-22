00b55c1070c6691491edd57a7c6d1b09e9f90812
AlishaBeuria <alishabeuria905@gmail.com>
2024-10-22 18:33:37 +0530

    this my mern stack project

---
+++ Frontend/JavaScript/class6/function.js
@@ -0,0 +1,50 @@
+/*function:function is a set of block of reusable code
+to perform a specific task*/
+//function declaration 
+//function is a key word
+//function syntax(){
+//}
+
+
+//write a function print your name
+//function printName(){
+  //  console.log(`my name is alisha`);
+//}
+ 
+
+//printName();//invoke or call
+//Arguments and parameter
+//function printName(name){//name is the parameter
+ //console.log(`myname is ${name}`);
+//}
+//printName("payal");
+
+
+//age city
+//function printName(name,age,city){
+  //  console.log(`my name is${name},my age is ${age},my city is ${city}` );
+//}
+//printName("payal","22","bbsr")
+
+
+//function printName(name,age,city){
+   // console.log(`my name is${name}, ${age}, ${city}` );
+//}
+//printName("payal","22","bbsr")
+
+
+//invoke or call //printName is the caller
+//let output=printName("payal","22","bbsr");
+//console.log(output);
+
+
+//syntax:
+const printName=(name)=>{//const for constant value//=> is fat arrow
+    return`my name is ${name}`;
+
+}
+console.log(printName("alisha"));//call
+//reference error means this is variable is not stored at the memory
+
+//done with   basics of function
+//callback 
